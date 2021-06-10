0c81d1704453a048e16dc2056b9a5ec6973c21d4
sanjana18tyagi <81044148+sanjana18tyagi@users.noreply.github.com>
2021-06-09 21:01:29 -1200

    detection

---
+++ detection.py
@@ -0,0 +1,219 @@
+import os
+import re
+import string
+
+import pandas as pd
+import numpy as np
+
+from collections import Counter
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import regularizers
+from tensorflow.keras import preprocessing
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import re 
+import tweepy 
+from tweepy import OAuthHandler 
+from textblob import TextBlob 
+
+import warnings
+warnings.filterwarnings('ignore')
+
+main_data=pd.read_csv("train.csv")
+data=main_data.copy()
+data.drop(columns=['id'],axis=1,inplace=True)
+data
+
+def clean_tweet(tweet):
+    tweet = re.sub("#", "",tweet) # Removing '#' from hashtags
+    tweet = re.sub("[^a-zA-Z#]", " ",tweet) # Removing punctuation and special characters
+    tweet = re.sub(r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',"<URL>", tweet)
+    tweet = re.sub('http','',tweet)
+    tweet = re.sub(" +", " ", tweet)
+    tweet = tweet.lower()
+    return tweet
+data["tweet"]=data["tweet"].apply(clean_tweet)
+data
+
+#Check class distribution in dependent variable 
+display(data['label'].value_counts().to_frame())
+
+#Balancing the dataset using Oversampling
+data1=data[data['label']==1]
+data0=data[data['label']==0]
+data=pd.concat([data,data1,data1], axis=0)
+display(data['label'].value_counts().to_frame())
+
+#preprocess train dataset
+
+data['tweet'] = data['tweet'].apply(clean_tweet)
+data['Num_words_text'] = data['tweet'].apply(lambda x:len(str(x).split())) 
+
+train_data,test_data= train_test_split(data, test_size=0.2)
+train_data.reset_index(drop=True,inplace=True)
+test_data.reset_index(drop=True,inplace=True)
+data
+
+#classes proportion in dependent variable in train and test dataset
+print('Train Data')
+print(train_data['label'].value_counts())
+print(len(train_data))
+print('Test Data')
+print(test_data['label'].value_counts())
+print(len(test_data))
+
+#train and validation dataset splitting
+X_train, X_valid, y_train, y_valid = train_test_split(train_data['tweet'].tolist(),\
+                                                      train_data['label'].tolist(),\
+                                                      test_size=0.2,\
+                                                      stratify = train_data['label'].tolist(),\
+                                                      random_state=0)
+
+
+print('Train data len:'+str(len(X_train)))
+print('Class distribution'+str(Counter(y_train)))
+print('Valid data len:'+str(len(X_valid)))
+print('Class distribution'+ str(Counter(y_valid)))
+
+num_words = 50000
+
+tokenizer = Tokenizer(num_words=num_words,oov_token="unk")
+tokenizer.fit_on_texts(X_train)
+
+#convert sentences to sequences of numbers
+x_train = np.array( tokenizer.texts_to_sequences(X_train) )
+x_valid = np.array( tokenizer.texts_to_sequences(X_valid) )
+x_test  = np.array( tokenizer.texts_to_sequences(test_data['tweet'].tolist()) )
+#padding 
+maxlen=50
+x_train = pad_sequences(x_train, padding='post', maxlen=maxlen)
+x_valid = pad_sequences(x_valid, padding='post', maxlen=maxlen)
+x_test = pad_sequences(x_test, padding='post', maxlen=maxlen)
+
+
+train_labels = np.asarray(y_train)
+valid_labels = np.asarray(y_valid)
+test_labels = np.asarray(test_data['label'].tolist())
+print('Train data len:'+str(len(x_train)))
+print('Class distribution'+str(Counter(train_labels)))
+
+print('Validation data len:'+str(len(x_valid)))
+print('Class distribution'+str(Counter(valid_labels)))
+
+print('Test data len:'+str(len(x_test)))
+print('Class distribution'+str(Counter(test_labels)))
+
+#tensorflow dataset preparation
+train_ds = tf.data.Dataset.from_tensor_slices((x_train,train_labels))
+valid_ds = tf.data.Dataset.from_tensor_slices((x_valid,valid_labels))
+test_ds = tf.data.Dataset.from_tensor_slices((x_test,test_labels))
+
+#model preparation
+max_features =50000
+embedding_dim =16
+sequence_length = maxlen
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Embedding(max_features +1, embedding_dim, input_length=sequence_length,\
+                                    embeddings_regularizer = regularizers.l2(0.005))) 
+model.add(tf.keras.layers.Dropout(0.4))
+
+model.add(tf.keras.layers.LSTM(embedding_dim,dropout=0.2, recurrent_dropout=0.2,return_sequences=True,\
+                                                             kernel_regularizer=regularizers.l2(0.005),\
+                                                             bias_regularizer=regularizers.l2(0.005)))
+
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(512, activation='relu',\
+                                kernel_regularizer=regularizers.l2(0.001),\
+                                bias_regularizer=regularizers.l2(0.001),))
+model.add(tf.keras.layers.Dropout(0.4))
+
+model.add(tf.keras.layers.Dense(8, activation='relu',\
+                                kernel_regularizer=regularizers.l2(0.001),\
+                                bias_regularizer=regularizers.l2(0.001),))
+model.add(tf.keras.layers.Dropout(0.4))
+model.add(tf.keras.layers.Dense(1,activation='sigmoid'))
+                               
+model.summary()
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(),optimizer=tf.keras.optimizers.Adam(1e-3),metrics=[tf.keras.metrics.BinaryAccuracy()])
+
+epochs = 10
+# Fit the model using the train and test datasets.
+history = model.fit(train_ds.shuffle(5000).batch(1024),
+                    epochs= epochs ,
+                    validation_data=valid_ds.batch(1024),
+                    verbose=1)
+
+#model's metrics on test dataset
+x_test  = np.array( tokenizer.texts_to_sequences(test_data['tweet'].tolist()) )
+x_test = pad_sequences(x_test, padding='post', maxlen=maxlen)
+
+
+#Generate predictions for all samples
+predictions = model.predict(x_test)
+#accessing test file
+final_test=pd.read_csv("test.csv")
+ftest=final_test.copy()
+ftest.drop(columns=['id'],axis=1,inplace=True)
+ftest['tweet'] = ftest['tweet'].apply(clean_tweet)
+f_test  = np.array( tokenizer.texts_to_sequences(ftest['tweet'].tolist()) )
+f_test = pad_sequences(f_test, padding='post', maxlen=maxlen)
+f_test
+
+#make predictions on validation dataset
+valid_predict= model.predict(x_valid)
+print(valid_predict[:10])
+
+cutoff=0.86
+test_data['pred_sentiment']= predictions
+test_data['pred_sentiment'] = np.where((test_data.pred_sentiment >= cutoff),1,test_data.pred_sentiment)
+test_data['pred_sentiment'] = np.where((test_data.pred_sentiment < cutoff),0,test_data.pred_sentiment)
+
+#decide the cutoff for classifying the predicted probabilities as 1 or 0
+def plot_roc(name, labels, predictions, **kwargs):
+    fp, tp, thresholds = sklearn.metrics.roc_curve(labels, predictions)
+    plt.plot(fp, tp, label=name, linewidth=2, **kwargs)
+    plt.xlabel('False positives Rate')
+    plt.ylabel('True positives Rate')
+    plt.xlim([-0.03, 1.0])
+    plt.ylim([0.0, 1.03])
+    plt.grid(True)
+    thresholdsLength = len(thresholds)
+    thresholds_every = 1000
+    colorMap = plt.get_cmap('jet', thresholdsLength)
+    for i in range(0, thresholdsLength, thresholds_every):
+        threshold_value_with_max_four_decimals = str(thresholds[i])[:5]
+        plt.text(fp[i] - 0.03, tp[i] + 0.001, threshold_value_with_max_four_decimals, fontdict={'size': 15}, color=colorMap(i/thresholdsLength));
+
+    ax = plt.gca()
+    ax.set_aspect('equal')
+mpl.rcParams['figure.figsize'] = (7,7)
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+plot_roc("Valid Baseline", valid_labels, valid_predict, color=colors[0], linestyle='--')
+plt.legend(loc='lower right')
+labels = [0, 1]
+print(classification_report(test_data['label'].tolist(),test_data['pred_sentiment'].tolist(),labels=labels))
+
+#predict on actual test data
+predictions = model.predict(f_test)
+
+#mapping prediction to 1 or 0
+ftest['pred_sentiment']= predictions
+ftest['pred_sentiment'] = np.where((ftest.pred_sentiment < cutoff),1,test.pred_sentiment)
+
+#tweets - offensive 
+ftest[ftest['pred_sentiment']==0]
+© 2021 GitHub, Inc.
